1e64a9fba4ea87626b63c334c93a9e7183ad191b
JoWaBo92 <jonas.wagner@rub.de>
2026-01-26 13:19:34 +0100

    feat: normalization stage

--- domain/preprocessing.py
+++ domain/preprocessing.py
@@ -1,14 +1,35 @@
+from abc import ABC, abstractmethod
+import unicodedata
+import re
+
+from domain.source_document import ASRExtract, ManualTranscript
+
 class PreprocessingPipeline:
     pass
 
-class PipelineStage:
-    pass
+class PipelineStage(ABC):
+    @abstractmethod
+    def apply(self):
+        pass
 
 class TokenizationStage(PipelineStage):
-    pass
+    def apply(self):
+        pass
 
-class NormalizaionStage(PipelineStage):
-    pass
+class NormalizationStage(PipelineStage):
+    def apply(self, input):
+
+        # Normalize unicode
+        s = unicodedata.normalize("NFC", input)
+
+        # Normalize whitespaces
+        s = s.replace("\u00A0", " ")
+        s = re.sub(r"\s+", " ", s).strip()
+
+        # Convert to lower case
+        s = s.lower()
+
+        return s
 
 class PreprocessingResult:
     pass
@@ -17,5 +38,15 @@ class PreprocessingConfig:
     pass
 
 class Token:
-    pass
+    text: str
+    index: int
 
+if __name__ == "__main__":
+    extract = ASRExtract("FaPra Timealignment\ADG3149_01_01_de_speaker.csv")
+    transcript = ManualTranscript("FaPra Timealignment\ADG3149_01_01.odt")
+
+    norm = NormalizationStage()
+
+    print(extract.segments[0].text)
+
+    print(norm.apply(extract.segments[0].text))
